0d57c4e10e7163e531d4a695bd1ebac36460b2df
Maria <maria.alcocer@fundacion-jala.org>
2015-11-16 20:53:59 -0400

    Task 1: - sumatory - min - max - average

--- training/js/main.js
+++ training/js/main.js
@@ -21,7 +21,54 @@ var getAvg = function(collection, cant){
     }
 
 };
-/*var getSum = function(collection, cant){
+var getMajor = function(collection, cant, maxi){
+    var major = maxi;
+    if(cant > 0) {
+       if(major > collection[cant-1])
+       {
+
+           return getMajor(collection, cant - 1, major)
+       }
+        else
+       {
+           major = collection[cant-1];
+           return getMajor(collection, cant - 1, collection[cant-1])
+       }
+    }
+    else
+    {
+        return major;
+    }
+
+};
+var getMenor = function(collection, cant, mini){
+    if (mini == 0)
+    {
+        var menor = collection[cant-1];
+    }
+    else
+    {
+        var menor = mini;
+    }
+    if(cant > 0) {
+        if(menor < collection[cant-1])
+        {
+
+            return getMenor(collection, cant - 1, menor)
+        }
+        else
+        {
+            menor = collection[cant-1];
+            return getMenor(collection, cant - 1, collection[cant-1])
+        }
+    }
+    else
+    {
+        return menor;
+    }
+
+};
+var getSum = function(collection, cant){
 
     if(cant > 0) {
         return getSum(collection, cant - 1) + collection[cant - 1];
@@ -32,8 +79,10 @@ var getAvg = function(collection, cant){
     }
 
     };
-console.log(getSum([5,3,4,9,2], 5));*/
+console.log(getSum([5,3,4,9,2], 5));
 console.log(getAvg([3,3,3], 3));
+console.log(getMajor([8,2,3], 3, 0));
+console.log(getMenor([8,2,3], 3, 0));
 
 
 var isCapicua = function(num){

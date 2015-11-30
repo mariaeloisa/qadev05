f69604dc8eeef6b7565519cd1d6bf034b9f14c18
Maria <maria.alcocer@fundacion-jala.org>
2015-11-30 14:55:19 -0400

    Test1

---
+++ training/js/createTable.js
@@ -0,0 +1,10 @@
+/**
+ * Created by MariaAlcocer on 11/27/2015.*/
+var createOneTable= function(sizeRow , sizeColumn){
+    var table = document.createElement('table');
+    for (var i = 0 ; i < sizeRow ; i ++)
+    {
+
+    }
+
+};

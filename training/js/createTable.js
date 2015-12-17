fd1bad9636bf3853dc8db35dbfd2f3ac27d97e92
Maria <maria.alcocer@fundacion-jala.org>
2015-12-17 15:43:06 -0400

    test

--- training/js/createTable.js
+++ training/js/createTable.js
@@ -8,3 +8,43 @@ var createOneTable= function(sizeRow , sizeColumn){
     }
 
 };
+
+var tab = createOneTable(2,2);
+
+//crear con jquery tabla
+
+style = '<style>.cell{background: green; width: 50px; display: inline-block;}'+
+'.container {background-color:#Fc0; width:159px;}</<style>';
+
+var table = '<div class="container">'+
+'    <div class="cell"><div>'+
+'    <div class="cell"><div>'+
+'    <div class="cell"><div>'+
+'   <div class="cell"><div>'+
+'</div>';
+
+var drawField = function (size){
+    var style = '<style>.cell{background: green; width: 50px; display: inline-block;}'+
+        '.container {background-color:#Fc0; width:159px;}</style>';
+    var containerStr = '<div class="container"></div>';
+    var cellStr = '<div class="cell"></div>';
+    var styleEl = $(style); //html element
+    var containerEl = $(containerStr); //html element
+
+    for(var i = 0, total=size*size; i < total; i++)
+    {
+        var cellEl = $(cellStr);//html element
+        cellEl.on('click', function(e){
+        console.log(e);
+        console.log('Hello! you clicked on a cell');
+        $(e.target).css('background','red');
+        });
+        containerEl.append(cellEl);
+    }
+    $('body').html(styleEl);
+    $('body').append(containerEl);
+    //se puede usar el .html lo que hace es borrar todo el documento y reemplazr lo que hay en mi codigo
+    //$('body').html(styleEl);
+
+};
+//NO FUNCIONA!!!
